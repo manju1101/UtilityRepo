a7e0f71c769d909384f294310a9e293d4f362382
manju1101 <45060095+manju1101@users.noreply.github.com>
2019-01-24 10:50:39 +0530

    Update reUsableScripts.groovy

--- LinuxSystem/reUsableScripts.groovy
+++ LinuxSystem/reUsableScripts.groovy
@@ -1,5 +1,5 @@
 def triggerEmail(){
-emailext body: "${currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\n More info at: ${env.BUILD_URL}",
+emailext attachLog: true, body: "${currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\n More info at: ${env.BUILD_URL}",
                     recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']],
                     subject: "Jenkins Build ${currentBuild.currentResult}: Job ${env.JOB_NAME}"
                     
